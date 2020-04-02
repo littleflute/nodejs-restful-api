5280656d671d4c8dc3c07e289b250710fa036a97
littleflute <littleflute@163.com>
2020-04-01 23:29:07 -0700

    test1

--- db.js
+++ db.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://yourMongoDBURIGoesHere');
+mongoose.connect('mongodb+srv://yongling:<yongling>@cluster0-uam3v.mongodb.net/test?retryWrites=true&w=majority');
